d6743c4a9c65f0369019e2d7133846a25aec74f9
AmethystOrchid <103237374+AmethystOrchid@users.noreply.github.com>
2022-12-06 13:25:16 +0800

    Added print statements for 1D and 2D containers

--- AoCUtility.h
+++ AoCUtility.h
@@ -1,6 +1,8 @@
 #ifndef AOCUTIL
 #define AOCUTIL
 
+#define varName(v) (#v)
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -10,6 +12,8 @@
 #include <algorithm>
 #include <cmath>
 #include <numeric>
+#include <typeinfo>
+#include <unordered_set>
 
 using namespace std;
 
@@ -23,6 +27,26 @@ vector<string> readLines(string filename)
     while (getline(file, line)) {result.push_back(line);}
 
     return result;
+}
+
+template <typename T>
+void print1Dcontainer(T input)
+{
+    cout << "\n=======================\n";
+    cout << "Printing " << typeid(input).name() << "\n";
+    cout << varName(input) << ": ";
+    for (auto i : input) {cout << i << " ";}
+    cout << "\n=======================\n";
+}
+
+template <typename T>
+void print2Dcontainer(T input)
+{
+    cout << "\n=======================\n";
+    cout << "Printing " << typeid(input).name() << "\n";
+    cout << varName(input) << ": \n";
+    for (auto i : input) {for (auto j : i) {cout << j << " ";} cout << "\n";} 
+    cout << "\n=======================\n";
 }
 
 template <typename T>
